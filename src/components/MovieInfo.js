f4584f3cef3de17c4735f651168a6b6d81f9b040
Arda Yılmaz <arday2431@gmail.com>
2021-06-21 22:50:19 +0300

    responsive tasarım düzenlendi

--- src/components/MovieInfo.js
+++ src/components/MovieInfo.js
@@ -1,19 +1,23 @@
-import '../styles/moviepage.scss'
+import "../styles/moviepage.scss";
 
-const MovieInfo = ({movie}) => {
+const MovieInfo = ({ movie }) => {
   return (
     <>
       <div className="movie-info">
-        <span className="release-date">Yayınlanma tarihi: {movie.release_date}</span>
-        <div className="movie-image">
-          <img
-            src={`https://www.themoviedb.org/t/p/w600_and_h900_bestv2${movie.backdrop_path}`}
-            alt={movie.title}
-          />
-        </div>
-        <div className="info">
-          <div className="description">
-            <p>{movie.overview}</p>
+        <span className="release-date">
+          Yayınlanma tarihi: {movie.release_date}
+        </span>
+        <div className="movie-detail">
+          <div className="movie-image">
+            <img
+              src={`https://www.themoviedb.org/t/p/w600_and_h900_bestv2${movie.backdrop_path}`}
+              alt={movie.title}
+            />
+          </div>
+          <div className="info">
+            <div className="description">
+              <p>{movie.overview}</p>
+            </div>
           </div>
         </div>
       </div>
@@ -21,4 +25,4 @@ const MovieInfo = ({movie}) => {
   );
 };
 
-export default MovieInfo
+export default MovieInfo;
